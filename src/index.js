298e3096c6c37b01c79bd2e0c1e9694797327317
Iryna Zalata <iryna.zalata@gmail.com>
2020-10-29 20:06:43 +0200

    Add completed task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import './styles.css';
-import './fetchCountries.js';
 import fetchCountries from './fetchCountries.js';
 import debounce from '../node_modules/lodash.debounce/index.js';
 
-const list = document.querySelector('.countries-list');
-
-
-
 document.querySelector('input').addEventListener(
 'input',
-  debounce(fetchCountries, 500),
+  debounce(fetchCountries, 800),
 )
 
